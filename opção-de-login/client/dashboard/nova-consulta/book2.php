43e9040193b7e4a2239af0e3318f72e2507b935a
Kennedy Marques <kennedyleandroam@gmail.com>
2023-11-15 18:15:26 -0300

    design nova consulta e consultas marcadas

--- opção-de-login/client/dashboard/nova-consulta/book2.php
+++ opção-de-login/client/dashboard/nova-consulta/book2.php
@@ -1,48 +1,45 @@
 <?php
 session_start();
-if(isset($_GET["selectclinic"]) && !empty($_GET["clinicid"]))
-{
-    $_SESSION["clinicid"]=$_GET['clinicid'];
+if (isset($_GET["selectclinic"]) && !empty($_GET["clinicid"])) {
+    $_SESSION["clinicid"] = $_GET['clinicid'];
 }
 require("../../conexões/db.php");
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
-<body>
-<div class="container-fluid pt-2 pb-2" style="background-color:rgba(209, 209, 170,0.3);">
-    <div class="row">
-        <div class="col-sm-10 ">
-        <a href="dashboard.php"> <img  width="150" class="ml-0 mt-2 mb-2" src="../images/logo.png"> </a>
-        </div>
-        <div class="col-sm-2">
-            <h3 class="mr-0">Olá, <?php echo $_SESSION['nome']; ?>!</h3>
-            <a href="logout.php" class="btn btn-primary ">Sair</a>
-        </div>
-    </div>
-</div>
-<div class="container-fluid text-center" style="background-color:rgb(176, 192, 236);">
-    <div class="">
-        <h2 class="pt-3 pb-3">Fisioterapeutas disponiveis</h2>
-    </div>
-</div>
-    <?php
-    if(isset($_GET["selectclinic"]) && isset($_GET['clinicid']))
-    {
 
-        $sql="SELECT * FROM `logind` WHERE clinicid=".$_SESSION['clinicid'];
+<body>
+    <div class="container-fluid pt-2 pb-2" style="background-color: #113838">
+        <div class="row">
+            <div class="col-sm-10 ">
+                <img width="200" class="ml-0 mt-2 mb-2" src="../consultas-marcadas/default_transparent.png">
+            </div>
+            <div class="col-sm-2">
+                <h3 class="mr-0 text-white">Olá, <?php echo $_SESSION['nome']; ?>!</h3>
+                <a href="../dashboard.php" class="btn btn-outline-light">Painel do Paciente</a>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid text-center" style="background-color:#79e3bd">
+        <div class="">
+            <h2 class="pt-3 pb-3">Fisioterapeutas disponiveis</h2>
+        </div>
+    </div>
+    <?php
+    if (isset($_GET["selectclinic"]) && isset($_GET['clinicid'])) {
+
+        $sql = "SELECT * FROM `logind` WHERE clinicid=" . $_SESSION['clinicid'];
         $res = mysqli_query($con, $sql);
-        if($res)
-        {   
-            if (mysqli_num_rows($res))
-            {
+        if ($res) {
+            if (mysqli_num_rows($res)) {
                 echo "<form action=book3.php class='text-center'>";
                 echo "<div class='table-responsive'>
                     <table class='table table-hover'>
@@ -60,37 +57,31 @@ require("../../conexões/db.php");
                         <th> Salário </th>
                       </tr>
                       </thead>";
-                while($row = mysqli_fetch_assoc($res))
-                {
+                while ($row = mysqli_fetch_assoc($res)) {
                     echo "<tr> 
-                                <td> <input type=radio name=docid value=".$row['doctorid']."></td>
-                                <td>". $row["doctorname"]." </td>
-                                <td>". $row["doctoremail"]." </td>
-                                <td>". $row["phoneno"]."</td>
-                                <td>".$row["speciality"]." </td>
-                                <td>".$row["rating"]." </td>
-                                <td>". $row["degree"]."</td>
-                                <td>".$row["experience"]." </td>
-                                <td>".$row["age"]." </td>
-                                <td>".$row["fee"]." </td>
+                                <td> <input type=radio name=docid value=" . $row['doctorid'] . "></td>
+                                <td>" . $row["doctorname"] . " </td>
+                                <td>" . $row["doctoremail"] . " </td>
+                                <td>" . $row["phoneno"] . "</td>
+                                <td>" . $row["speciality"] . " </td>
+                                <td>" . $row["rating"] . " </td>
+                                <td>" . $row["degree"] . "</td>
+                                <td>" . $row["experience"] . " </td>
+                                <td>" . $row["age"] . " </td>
+                                <td>" . $row["fee"] . " </td>
                         </tr> ";
-
                 }
 
                 echo "</table> </div> <br> <br> <div class=ip> ";
-                echo " <button type='submit' class='btn btn-primary ml-auto' value='SELECTDOCTOR' name='selectdoctor'>Selecionar Doutor</button>";
+                echo " <button type='submit' class='btn btn-dark ml-auto' value='SELECTDOCTOR' name='selectdoctor'>Selecionar Doutor</button>";
                 echo "</form> </div>";
-
-            }
-            else{
+            } else {
                 echo " <h1> 0 resultados </h1>";
             }
         }
-
-
-
     }
     ?>
-    
+
 </body>
+
 </html>
